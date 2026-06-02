dcd25d762126fa9e3b63bdc12140cfd4162ef7e8
dazome <zoha.b.ap@gmail.com>
2026-06-02 03:37:45 +0330

    using props

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Product from './Product/Product';
 function App() {
   return (
     <div className='container' >
-     <Product/>
-     <Product/>
-     <Product/>
+     <Product pic="pic1.jpg" name="asus" price={120} description="Its an asus laptop"/>
+     <Product pic="pic2.jpg" name="acer" price={220} description="Its an acer laptop"/>
+     <Product pic="pic3.png" name="hp" price={480} description="Its an lenovo laptop"/>
     </div>
   );
 }
--- src/Product/Product.js
+++ src/Product/Product.js
@@ -1,16 +1,17 @@
 import './Product.css'
-function Product(){
+function Product(props){
     return(
         <div>
             <h2 className='fh'>Product card</h2>
             <div className="card">
                 
-                <img src="pic1.jpg" alt="Product"/>
-                <h1>Dandelion</h1>
-                <p className="price">500$</p>
-                <p>It's the best flower</p>
+                <img src={props.pic} alt="Product" height={250}/>
+                <h1>{props.name}</h1>
+                <p className="price">${props.price}</p>
+                <p>{props.description}</p>
                 <p><button>Add To Cart</button></p>
             </div>
+           
         </div>
     )
 }
