7124a5be5ca10571cd50284ef320e9b5357ff222
dazome <zoha.b.ap@gmail.com>
2026-06-02 15:12:53 +0330

    using spread for props

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import './App.css';
 import Product from './Product/Product';
 
 function App() {
+  const allproduct=[
+    {id:1, pic:'pic1.jpg', name:'Asus', price:130, description:'Its an asus laptop', count:26},
+    {id:2, pic:'pic2.jpg', name:'Acer', price:220, description:'Its an acer laptop', count:30},
+    {id:3, pic:'pic3.png', name:'Lenovo', price:480, description:'Its a lenovo laptop', count:42}
+  ]
   return (
     <div className='container' >
-     <Product pic="pic1.jpg" name="asus" price={120} description="Its an asus laptop"/>
-     <Product pic="pic2.jpg" name="acer" price={220} description="Its an acer laptop"/>
-     <Product pic="pic3.png" name="hp" price={480} description="Its an lenovo laptop"/>
+     <Product {...allproduct[0]}/>
+     <Product {...allproduct[1]}/>
+     <Product {...allproduct[2]}/>
     </div>
   );
 }
--- src/Product/Product.js
+++ src/Product/Product.js
@@ -1,7 +1,25 @@
 import './Product.css'
 function Product(props){
+    // if(props.count>0){
+    // return(
+        // <div>
+        //     <h2 className='fh'>Product card</h2>
+        //     <div className="card">
+                
+        //         <img src={props.pic} alt="Product" height={250}/>
+        //         <h1>{props.name}</h1>
+        //         <p className="price">${props.price}</p>
+        //         <p>{props.description}</p>
+        //         <p><button>Add To Cart</button></p>
+        //     </div>
+           
+        // </div>
+    // )}
+    // return null
+
     return(
-        <div>
+        props.count &&
+    <div>
             <h2 className='fh'>Product card</h2>
             <div className="card">
                 
